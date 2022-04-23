85e27403ae62dac4f6325dc73fd9dd865d924611
Minsiki <43837214+Minsiki@users.noreply.github.com>
2022-04-24 01:11:44 +0900

    public 메소드 private으로 수정

--- src/js/components/WinningLottoForm.js
+++ src/js/components/WinningLottoForm.js
@@ -2,24 +2,27 @@ import LottoNumber from "../domain/LottoNumber.js";
 import WinningNumbers from "../domain/WinningNumbers.js";
 
 export default class WinningLottoForm {
+    $winningNumbers;
+    $bonusNumber;
+
     constructor(props) {
         this.$winngingArea = document.querySelector("#lotto-winning-area");
         this.props = props;
     }
 
     render() {
-        this.$winngingArea.innerHTML = this.getWinningTemplate();
+        this.$winngingArea.innerHTML = this.#getWinningTemplate();
     }
 
     mounted() {
-        document.querySelector("#open-result-modal-button").addEventListener("click", () => {
-            this.onClickResultModalButton();
-        });
+        document
+            .querySelector("#open-result-modal-button")
+            .addEventListener("click", () => this.#onClickResultModalButton());
         this.$winningNumbers = document.querySelectorAll(".winning-number");
         this.$bonusNumber = document.querySelector(".bonus-number");
     }
 
-    onClickResultModalButton() {
+    #onClickResultModalButton() {
         try {
             const winningNumbers = Array.from(this.$winningNumbers)
                 .filter((data) => {
@@ -35,7 +38,7 @@ export default class WinningLottoForm {
         }
     }
 
-    getWinningTemplate() {
+    #getWinningTemplate() {
         return `
         <form class="mt-9">
             <label class="flex-auto d-inline-block mb-3">지난 주 당첨번호 6개와 보너스 넘버 1개를 입력해주세요.</label>
@@ -44,10 +47,7 @@ export default class WinningLottoForm {
                     <h4 class="mt-0 mb-3 text-center">당첨 번호</h4>
                     <div>
                         ${Array.from({ length: LottoNumber.LOTTO_LENGTH })
-                            .map(
-                                () =>
-                                    `<input type="number" class="winning-number mx-1 text-center"/>`
-                            )
+                            .map(() => `<input type="number" class="winning-number mx-1 text-center"/>`)
                             .join("")}
                     </div>
                 </div>
